3747108c845a08b988cc92d5750e202a3959cc1f
luck1392444731 <luck1392444731@163.com>
2023-05-11 22:40:00 +0800

    vite Internal server error

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,29 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    // host: 'localhost',
+    // https: false, //是否启用 http 2
+    // cors: true, //为开发服务器配置 CORS , 默认启用并允许任何源
+    // open: true, //服务启动时自动在浏览器中打开应用
+    // port: '9000',
+    // strictPort: false, //设为true时端口被占用则直接退出，不会尝试下一个可用端口
+    // force: true, //是否强制依赖预构建
+    // hmr: false, //禁用或配置 HMR 连接
+    // // 传递给 chockidar 的文件系统监视器选项
+    // watch: {
+    //   ignored: ['!**/node_modules/your-package-name/**'],
+    // },
+    // 反向代理配置
+    proxy: {
+      '/api': {
+        target: 'https://api.imooc-admin.lgdsunday.club/',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
